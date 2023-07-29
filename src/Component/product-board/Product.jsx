0240333d25f6a07933ca446de0930ec4fc44f81c
DamifeZion <damifemartins@gmail.com>
2023-07-29 17:54:37 +0100

    Product height fixed to fit

--- src/Component/product-board/Product.jsx
+++ src/Component/product-board/Product.jsx
@@ -10,7 +10,7 @@ const Product = ({
   proStrikePrice,
 }) => {
   return (
-    <div className="product overflow-hidden cursor-pointer hover:scale-[1.01] rounded-md flex flex-col min-w-[42%] md:min-w-[24.3%] lg:min-w-[16.1%] h-[13rem] sm:min-h-[16.2rem] smlg:h-[19.8rem] lg:h-[16.8rem] relative gap-2">
+    <div className="product cursor-pointer hover:scale-[1.01] rounded-md flex flex-col min-w-[42%] md:min-w-[24.3%] lg:min-w-[16.1%] min-h-[13rem] sm:min-h-[16.2rem] smlg:min-h-[10.8rem] relative gap-2">
       <p
         style={proDiscountStyle}
         className=" absolute top-1 md:top-2 right-1 md:right-2 z-[1] px-[.2rem] md:px-[.32rem] py-[.13rem] text-[--discount-txt-color] rounded-[0.2rem] text-[.65rem] md:text-[.85rem] font-normal bg-[--discount-bg-color]"
@@ -18,11 +18,11 @@ const Product = ({
         {proDiscount}
       </p>
 
-      <div className="w-full h-[75%]">
-        <img src={proImg} alt="P1 Img" className=" w-full h-full" />
+      <div className="w-full h-[80%]">
+        <img src={proImg} alt="P1 Img" className=" w-full h-full object-cover" />
       </div>
 
-      <li className="flex-col place-items-start text-[0.9rem] font-light px-2">
+      <li className="flex-col place-items-start text-[0.9rem] font-light px-2 h-[40%]">
         <p className=" mb-2 overflow-hidden whitespace-nowrap text-ellipsis w-full">
           {proDescription}
         </p>
